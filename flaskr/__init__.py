a10bee9a8580c484e2844269683e0d05b7e4d2eb
Yicheng Ye <ye.yicheng123@gmail.com>
2021-05-04 14:08:31 -0400

    v1

---
+++ flaskr/__init__.py
@@ -0,0 +1,53 @@
+import os
+
+from flask import Flask
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from flaskr.db import get_db
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/')
+    def index():
+        db = get_db()
+        posts = db.execute(
+            'SELECT * FROM post ORDER BY count DESC LIMIT 20'
+        ).fetchall()
+        return render_template('index.html', posts=posts)
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run()
+
